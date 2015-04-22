b38febea640c4692d77b3536355f24d62fc1c13d
jterwil2 <jterwil2@illinois.edu>
2015-04-21 20:25:29 -0500

    Chat is working

---
+++ OurMusic/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace OurMusic
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void Send(String name, String message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
